71e5c14f81dc5ae77af3ec23ce106a925f133b51
mark <mark@shavlik.us>
2016-08-20 12:08:21 -0500

    test

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,15 +1,15 @@
 #include "ofApp.h"
 #include <time.h>
 void toFile(string path, vector<std::pair<ofColor, int>>&dat) {
-	
-	
+
+
 	ofFile file(path);
 	file.open(path, ofFile::WriteOnly);
 	time_t rawtime;
 	time(&rawtime);
 
 	file << ctime(&rawtime) << "\n";
-	
+
 	int i = 1;
 	for (auto itr = dat.begin(); itr != dat.end(); ++itr) {
 		file << i << ":" << itr->first << ":" << itr->second << "\n";
@@ -22,7 +22,7 @@ void toFile(string path, vector<ofColor>&dat) {
 
 	ofFile file(path);
 	file.open(path, ofFile::WriteOnly);
-	
+
 	for (auto itr = dat.begin(); itr != dat.end(); ++itr) {
 		file << *itr << "\n";
 	}
@@ -30,7 +30,7 @@ void toFile(string path, vector<ofColor>&dat) {
 }
 void fromFile(string path, vector<ofColor> &dat) {
 	ofFile file(path);
-	
+
 	while (file) {
 		ofColor cur;
 		file >> cur;
@@ -67,18 +67,11 @@ bool test(unordered_map<int, int>&colors, ofColor&color, int i, int j, int k) {
 	testColor.b += k;
 	return find(colors, testColor, false);
 }
-<<<<<<< HEAD
 // return true if color added
 bool dedupe(unordered_map<int, int>&colors, ofColor&color, int rangeR, int rangeG, int rangeB) {
 
 	bool found = false;
-=======
-void dedupe(unordered_map<int, int>&colors, ofColor&color, int rangeR, int rangeG, int rangeB) {
 
-	bool found = false;
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
-	
 	for (int i = 0; i < rangeR && !found; ++i) {
 		for (int j = 0; j < rangeG && !found; ++j) {
 			for (int k = 0; k < rangeB && !found; ++k) {
@@ -119,7 +112,6 @@ void dedupe(unordered_map<int, int>&colors, ofColor&color, int rangeR, int range
 	}
 
 	if (!found) {
-<<<<<<< HEAD
 		std::unordered_map<int, int>::iterator got = colors.find(color.getHex());
 		if (got == colors.end()) {
 			// color is not in the list
@@ -153,15 +145,15 @@ void ofApp::readColors(unordered_map<int, int> &findDarkcolors, unordered_map<in
 	else {
 		// create it	
 		vector<ofColor> dat;
-		
+
 		for (int w = 0; w < image.getWidth(); w += 1) {
 			for (int h = 0; h < image.getHeight(); h += 1) {
 				ofColor color = image.getPixels().getColor(w, h);
-				 // bugbug ? save all colors so its easier to tweak data later? maybe a different file?
-				
+				// bugbug ? save all colors so its easier to tweak data later? maybe a different file?
+
 				bool found;
 				//if (color.r == 255 && color.g == 255 && color.b == 255) {
-					//continue; //ignore white need this to be the back ground color
+				//continue; //ignore white need this to be the back ground color
 				//}
 				if (!isCool(color) && color.getBrightness() > 200) {
 					warm = color;// go with most recent
@@ -181,17 +173,11 @@ void ofApp::readColors(unordered_map<int, int> &findDarkcolors, unordered_map<in
 		}
 		toFile(filename, dat);
 	}
-=======
-		find(colors, color, true);
-	}
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 }
 void ofApp::setup() {
-    cam.setup(640, 480);
+	cam.setup(640, 480);
 	//https://github.com/frauzufall/ofxGuiExtended
-	gui.setup(group, "setup",1000, 0);
-<<<<<<< HEAD
+	gui.setup(group, "setup", 1000, 0);
 	gui.add(threshold.set("Threshold", 5, 0.0, 255.0));
 	gui.add(targetColor.set("RGB", 128.0, 0.0, 300.0));
 	gui.add(generatecolors.set("genearate", true));
@@ -201,64 +187,26 @@ void ofApp::setup() {
 	bool b = image.load("photo2.jpg");//bugbug menu ize
 	image.resize(500, 500);
 	warm = ofColor::lightYellow;
-=======
-	gui.add(threshold.set("Threshold", 120, 0.0, 255.0));
-	gui.add(targetColor.set("RGB", 128.0, 0.0, 300.0));
-	gui.add(generatecolors.set("genearate", true));
-	
-	bool b = image.load("photo2.jpg");//bugbug menu ize
-	image.resize(500, 500);
-	
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	cv::Mat img2 = toCv(image);
 
 	cv::bilateralFilter(img2, img, 15, 80, 80);
-	
+
 	ofSetFrameRate(120);
 	ofxCv::toOf(img, image);
 	image.setImageType(OF_IMAGE_COLOR); // should not need this? TODO any over-head / conversion?
-<<<<<<< HEAD
 
 	unordered_map<int, int> findDarkcolors;
 	unordered_map<int, int> findBrightcolors;//bugbug warm/cool?
 	readColors(findDarkcolors, findBrightcolors);
-=======
-	unordered_map<int, int> findDarkcolors;
-	unordered_map<int, int> findBrightcolors;//bugbug warm/cool?
-	ofPixels pixels = image.getPixels();
-	// variables: filter, w,h and my compression, threshold, pic size, finder.setMinAreaRadius(5);
-	//finder.setMaxAreaRadius(150); warm/cool?
-	for (int w = 0; w < image.getWidth(); w += 1) {
-		for (int h = 0; h < image.getHeight(); h += 1) {
-			ofColor c = image.getPixels().getColor(w, h);
-			//if (!isCool(c)) {
-			if (c.r == 255 && c.g == 255 && c.b == 255) {
-				continue; //ignore white bugbug or add it once or something, its too dominate?
-			}
-			if (c.getBrightness() > 180) {
-				dedupe(findBrightcolors, c, 0, 0, 0);
-			}
-			else {
-				dedupe(findDarkcolors, c, 2,2,2);
-			}
-			continue;
-			//make a mid brightness
-		}
-	}
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
-	
+
 	for (auto itr = findDarkcolors.begin(); itr != findDarkcolors.end(); ++itr) {
 		ofColor c = ofColor::fromHex(itr->first);
 		std::pair<ofColor, int> pair(c, itr->second);
-		
+
 		pairsDark.push_back(pair);
-		
+
 	}
-<<<<<<< HEAD
 	// drop light and dark, use "if light then set lightthresh hold" cleans it all up
-=======
-	
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	vector< pair<ofColor, int> >::iterator it = pairsLight.begin();
 	for (auto itr = findBrightcolors.begin(); itr != findBrightcolors.end(); ++itr) {
 		ofColor c = ofColor::fromHex(itr->first);
@@ -281,7 +229,6 @@ void ofApp::setup() {
 
 	sort(pairsDark.begin(), pairsDark.end(), [=](std::pair<ofColor, int>& a, std::pair<ofColor, int>& b)
 	{
-<<<<<<< HEAD
 		// push less saturated back, then darks
 		if (a.first.getSaturation() == b.first.getSaturation()) {
 			if (a.first.getBrightness() == b.first.getBrightness()) {
@@ -291,71 +238,51 @@ void ofApp::setup() {
 		}
 		return a.first.getSaturation() < b.first.getSaturation();
 		/* for debug
-=======
-		return a.first.getSaturation() < b.first.getSaturation();
-		/* for debug 
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 		if (a.first.r == b.first.r)
-			if (a.first.g == b.first.g)
-				return a.first.b > b.first.b;
-			else
-				return  a.first.g > b.first.g;
+		if (a.first.g == b.first.g)
+		return a.first.b > b.first.b;
 		else
-			return  a.first.r > b.first.r;
+		return  a.first.g > b.first.g;
+		else
+		return  a.first.r > b.first.r;
 
-			*/
-		
+		*/
+
 
 	}
 	);
 	sort(pairsLight.begin(), pairsLight.end(), [=](std::pair<ofColor, int>& a, std::pair<ofColor, int>& b)
 	{
-<<<<<<< HEAD
 		if (a.first.getSaturation() == b.first.getSaturation()) {
 			return a.first.getBrightness() > b.first.getBrightness();
 		}
 		return a.first.getBrightness() > b.first.getBrightness();
-=======
-		return a.first.getSaturation() < b.first.getSaturation();
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	}
 	);
-	
+
 	//for debug more data toFile("lotsofdata2.dat", pairsLight);
-<<<<<<< HEAD
 	pairsCurrent = &pairsDark;
-=======
-	pairsCurrent = &pairsLight;
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	ofSetBackgroundColor(ofColor::white);
 	ofSetBackgroundAuto(false);
 }// 45shavlik11
-//http://www.creativeapplications.net/tutorials/arduino-servo-opencv-tutorial-openframeworks/
-//http://www.autobotic.com.my/ds3218-servo-control-angle-180-degree-25t-servo-armv
-<<<<<<< HEAD
-void ofApp::update() { 
-    cam.update();
-	image.update();
-
-=======
+ //http://www.creativeapplications.net/tutorials/arduino-servo-opencv-tutorial-openframeworks/
+ //http://www.autobotic.com.my/ds3218-servo-control-angle-180-degree-25t-servo-armv
 void ofApp::update() {
-    cam.update();
+	cam.update();
 	image.update();
-	
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
-        //convertColor(cam, gray, CV_RGB2GRAY);
-		//GaussianBlur(gray, gray, gray.getWidth());
-       // Canny(gray, edge, mouseX, mouseY, 3);
-		//Sobel(gray, sobel);
-		
-       // gray.update();
-       // sobel.update();
-        //edge.update();
 
-		
-	
+	//convertColor(cam, gray, CV_RGB2GRAY);
+	//GaussianBlur(gray, gray, gray.getWidth());
+	// Canny(gray, edge, mouseX, mouseY, 3);
+	//Sobel(gray, sobel);
+
+	// gray.update();
+	// sobel.update();
+	//edge.update();
+
+
+
 }
-<<<<<<< HEAD
 void ofApp::echo(ContourFinder& finder, bool cleanPixelsOnly) {
 
 	for (int j = 0; j < finder.getPolylines().size(); j++) {
@@ -384,29 +311,11 @@ void ofApp::echo(ContourFinder& finder, bool cleanPixelsOnly) {
 					hitPixels.insert(s);
 				}
 			}
-=======
-void ofApp::echo(ofColor&color, ContourFinder& finder) {
-	int sz = finder.getPolylines().size();
-	if (finder.getPolylines().size() > 0) {
-		//color.a = 100;
-		ofSetColor(color);
-
-		if (finder.getPolylines().size() > 1) {
-			ofTessellator tess;
-			ofMesh mesh;
-			//tess.tessellateToMesh(finder.getPolylines(), OF_POLY_WINDING_ODD, mesh, true);
-			//mesh.draw();
-			//mesh.drawFaces();
-
-		}
-		for (int j = 0; j < finder.getPolylines().size(); j++) {
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 			ofTessellator tess;
 			ofMesh mesh;
 			tess.tessellateToMesh(finder.getPolylines()[j].getSmoothed(2), OF_POLY_WINDING_ODD, mesh, true);
 			mesh.draw();
 			finder.getPolylines()[j].getSmoothed(2).draw();
-<<<<<<< HEAD
 
 		}
 
@@ -419,17 +328,7 @@ void ofApp::draw() {
 	ofSetColor(ofColor::white);
 	ofSetBackgroundColor(warm);//bugbug use lightest found color
 	gui.draw();
-	
-=======
-		}
-	}
-}
-void ofApp::draw() {
-	gui.draw();
-	
-	ofSetColor(targetColor);
-	ofDrawRectangle(0, 700, 64, 64);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
+
 
 	//ofFill();
 	//cam.draw(0, 0);
@@ -437,23 +336,18 @@ void ofApp::draw() {
 	//edge.draw(640, 0);
 	//sobel.draw(640, 480);
 	ofSetLineWidth(1);
-<<<<<<< HEAD
 
-=======
-	ofSetColor(255, 255, 255);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	if (1) {
 		once = true;
 		ofxCv::toOf(img, image);
 		image.draw(500, 0);
 	}
-	
+
 	ofPushStyle();
-<<<<<<< HEAD
 	count = pairsCurrent->size();
 	if (index >= pairsCurrent->size() && index != -1) {
 		index = 0;
-		
+
 		if (pairsCurrent == &pairsDark) {
 			pairsCurrent = &pairsLight;// flip to dark if currently light
 		}
@@ -467,65 +361,32 @@ void ofApp::draw() {
 	else {
 		threshold = 180;
 	}
-	
+
 
 	//ofTranslate(300, 0);
-=======
-	
-	if (index >= pairsCurrent->size() && index != -1) {
-		index = 0;
-		if (pairsCurrent == &pairsDark) {
-			threshold = 180;
-			pairsCurrent = &pairsLight;// flip to dark if currently light
-		}
-		else {
-			threshold = 10;
-			pairsCurrent = &pairsDark;//bugbug flip to dark if currently light
-
-		}
-
-	}
-	//ofTranslate(300, 0);
-	ofEnableBlendMode(OF_BLENDMODE_ALPHA);
-	
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	// less colors, do not draw on top of each other, find holes
 	while (pairsCurrent && index > -1 && index < pairsCurrent->size()) {
 
 		ofColor color;
 		color = (*pairsCurrent)[index++].first;
 		ContourFinder finder;
-<<<<<<< HEAD
 		finder.setMinAreaRadius(1);
-=======
-		finder.setMinAreaRadius(5);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 		finder.setMaxAreaRadius(150);
 		finder.setSimplify(true);
 		finder.setAutoThreshold(false);
-		
+
 		finder.setUseTargetColor(true);
-<<<<<<< HEAD
 		finder.setFindHoles(true);// matters
 		finder.setSortBySize(false);
 		// put all results in a vector of PolyLines, then sort by size, then draw
 		// 'b' moves index back by 10
-=======
-		finder.setFindHoles(true);
-		finder.setSortBySize(true);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 		//color = ofColor::yellow;
 		finder.setTargetColor(color, TRACK_COLOR_RGB);
 		finder.setThreshold(threshold);
 		finder.findContours(img);
 		if (finder.getPolylines().size() > 1) {
-<<<<<<< HEAD
 			ofSetColor(color);
 			echo(finder, pairsCurrent != &pairsDark);
-=======
-			//polys.insert( finder.getPolylines());
-			echo(color, finder);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 			targetColor = color;
 			savedcolors.push_back(color);
 			break;
@@ -534,24 +395,12 @@ void ofApp::draw() {
 			int i = 0;
 			targetColor = ofColor::black;
 		}
-	
+
 	}
-	
-<<<<<<< HEAD
+
 	ofPopStyle();
 	return;
 
-=======
-	
-	ofPopStyle();
-	return;
-
-	
-
-	
-	ofPopStyle();
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 }
 void ofApp::keyPressed(int key) {
 	if (key == ' ') {
@@ -561,12 +410,12 @@ void ofApp::keyPressed(int key) {
 		else {
 			savex = index;
 			index = -1;
-			
+
 			string filename = "data" + ofToString(savecount++);
 			filename += ".dat";
 			toFile(filename, savedcolors);
 			savedcolors.clear();
 		}
-		
+
 	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -4,11 +4,7 @@
 #include "ofxCv.h"
 #include "ofxGui.h"
 #include <unordered_map>
-<<<<<<< HEAD
 #include <unordered_set>
-=======
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 using namespace ofxCv;
 using namespace cv;
 
@@ -16,49 +12,34 @@ class Drawing {
 public:
 	ContourFinder contourFinder;
 	ofColor color;
-	
-	
+
+
 };
 class ofApp : public ofBaseApp {
 public:
-<<<<<<< HEAD
-	void echo(ContourFinder& finder, bool cleanPixelsOnly=false);
-=======
-	void echo(ofColor&color, ContourFinder& finder);
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
-    void setup();
-    void update();
-    void draw();
+	void echo(ContourFinder& finder, bool cleanPixelsOnly = false);
+	void setup();
+	void update();
+	void draw();
 	ofVideoGrabber cam;
-    ofImage gray, edge, sobel;
+	ofImage gray, edge, sobel;
 	ofImage image;
 	vector<Drawing> drawings;
 	void keyPressed(int key);
 	std::vector<std::pair<ofColor, int>> pairsDark;
 	std::vector<std::pair<ofColor, int>> pairsLight;
-	std::vector<std::pair<ofColor, int>> *pairsCurrent=nullptr;
-<<<<<<< HEAD
-=======
-	int index = 0;
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
+	std::vector<std::pair<ofColor, int>> *pairsCurrent = nullptr;
 	vector<ofPolyline> polys;
 	int savecount = 0;
 	cv::Mat img;
 	bool once = false;
-<<<<<<< HEAD
 	void readColors(unordered_map<int, int> &findDarkcolors, unordered_map<int, int>& findBrightcolors);
 	int savex;
 	vector<cv::Rect> bounding;
-=======
-	
-	int savex;
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 	ofxPanel gui;
 	ofParameterGroup group;
 	ofParameter<string> imagePath;
 	ofParameter<float> threshold;
-<<<<<<< HEAD
 	ofParameter<int> count;
 	ofParameter<int> index;
 	ofParameter<ofColor>targetColor;
@@ -66,12 +47,5 @@ public:
 	ofParameter<bool> generatecolors;
 	vector<ofColor> savedcolors; //bugbug make unique, right now will store dupes
 	ofColor warm;
-=======
-	ofParameter<ofColor>targetColor;
-	
-	ofParameter<bool> generatecolors;
-	vector<ofColor> savedcolors; //bugbug make unique, right now will store dupes
-
->>>>>>> e14c94331bd39f8af161c3af4e12158d13843509
 };
 
