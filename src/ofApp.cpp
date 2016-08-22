faa34a3224997b3ace9b0bd9d76333a4486f2509
mark <mark@shavlik.us>
2016-08-22 10:11:37 -0500

    moduarlize

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,18 @@
 #include <time.h>
 #include <algorithm>
 
-void toFile(string path, vector<std::pair<ofColor, int>>&dat) {
+void LiveArt::snapshot() {
+	savex = index;
+	index = -1;
+
+	string filename = "data" + ofToString(savecount++);
+	filename += ".dat";
+	toFile(filename, savedcolors, true);
+	savedcolors.clear();
+}
+
+
+void LiveArt::toFile(string path, vector<std::pair<ofColor, int>>&dat) {
 
 	ofFile file(path);
 	file.open(path, ofFile::WriteOnly);
@@ -18,7 +29,7 @@ void toFile(string path, vector<std::pair<ofColor, int>>&dat) {
 	}
 	file.close();
 }
-void toFile(string path, vector<ofColor>&dat, bool clear=false) {
+void LiveArt::toFile(string path, vector<ofColor>&dat, bool clear) {
 
 	ofFile file(path);
 	file.open(path, ofFile::WriteOnly);
@@ -30,7 +41,7 @@ void toFile(string path, vector<ofColor>&dat, bool clear=false) {
 	}
 	file.close();
 }
-void fromFile(string path, vector<ofColor> &dat) {
+void LiveArt::fromFile(string path, vector<ofColor> &dat) {
 	ofFile file(path);
 
 	while (file) {
@@ -40,11 +51,11 @@ void fromFile(string path, vector<ofColor> &dat) {
 	}
 }
 
-bool isCool(ofColor&color) {
+bool LiveArt::isCool(ofColor&color) {
 	float h = color.getHue();
 	return (h > 80 && h < 330);
 }
-bool ofApp::find(ofColor&color, bool add) {
+bool LiveArt::find(ofColor&color, bool add) {
 
 	map<int, colorData>::iterator itr = shapes.find(color.getHex());
 	if (itr != shapes.end()) {
@@ -60,7 +71,7 @@ bool ofApp::find(ofColor&color, bool add) {
 		return false;
 	}
 }
-bool ofApp::test(ofColor&color, int i, int j, int k) {
+bool LiveArt::test(ofColor&color, int i, int j, int k) {
 	ofColor testColor = color;
 	testColor.r += i;
 	testColor.g += j;
@@ -68,7 +79,7 @@ bool ofApp::test(ofColor&color, int i, int j, int k) {
 	return find(testColor, false);
 }
 // return true if color added
-bool ofApp::dedupe(ofColor&color, int rangeR, int rangeG, int rangeB) {
+bool LiveArt::dedupe(ofColor&color, int rangeR, int rangeG, int rangeB) {
 
 	bool found = false;
 
@@ -122,8 +133,9 @@ bool ofApp::dedupe(ofColor&color, int rangeR, int rangeG, int rangeB) {
 	return false;
 }
 
-void ofApp::readColors() {
-	string filename = "pic3.dat";
+void LiveArt::readColors() {
+	string filename = imagePath;
+	filename += ".pic.dat";
 	ofFile file(filename);
 	if (file.exists()) {
 		// use it
@@ -166,63 +178,58 @@ void ofApp::readColors() {
 				//bugbug make a mid brightness
 			}
 		}
-		toFile(filename, dat);
+		toFile(filename, dat, false);
 	}
 }
-void ofApp::setup() {
-	cam.setup(640, 480);
+
+void LiveArt::setMenu(ofxPanel &gui) {
 	//https://github.com/frauzufall/ofxGuiExtended
-	gui.setup(group, "setup", 1000, 0);
-	gui.add(threshold.set("Threshold", 5, 0.0, 255.0));
-	gui.add(targetColor.set("RGB", 128.0, 0.0, 300.0));
-	gui.add(count.set("count", 0));
-	gui.add(index.set("current", 0));
+	ofParameterGroup realtime;
+	realtime.add(targetColor.set("RGB", 128.0, 0.0, 300.0));
+	realtime.add(count.set("count", 0));
+	realtime.add(index.set("current", 0));
+	realtime.add(targetColor.set("target color", ofColor::white));
+	realtime.add(warm.set("tone", ofColor::black));
 
-	ofSetFrameRate(120);
+	gui.setup(realtime, "setup", 1000, 0);
 
+	ofParameterGroup settings;
+	gui.add(settings);
+
+	settings.add(threshold.set("Threshold", 5, 0.0, 255.0));
+
+
+	settings.add(minRadius.set("minRadius", 1, 0.0, 255.0));
+	settings.add(maxRadius.set("maxRadius", 1, 0.0, 255.0));
+	settings.add(findHoles.set("findHoles", true));
+	settings.add(smoothingSize.set("smoothingSize", 2, 0.0, 255.0));
+	settings.add(smoothingShape.set("smoothingShape", 0.0, 0.0, 255.0));
+	settings.add(xImage.set("xImage", 500,20, 4000.0));
+	settings.add(yImage.set("yImage", 500, 20, 4000.0));
+	settings.add(d.set("d", 15, 0.0, 255.0));
+	settings.add(sigmaColor.set("sigmaColor", 80, 0.0, 255.0));
+	settings.add(sigmaSpace.set("sigmaSpace", 80, 0.0, 255.0));
+	settings.add(imagePath.set("imagePath"));
+	settings.add(imagePath.set("imagePath"));
+	settings.add(imagePath.set("imagePath"));
+	settings.add(imagePath.set("imagePath"));
+
+}
+void LiveArt::setup() {
 	bool b = image.load(imagePath);//bugbug menu ize
 
 	image.resize(xImage, yImage);
 	warm = ofColor::lightYellow; // default
 	cv::Mat img2 = toCv(image);
-	cv::bilateralFilter(img2, img, d, sigmaColor, sigmaSpace); 
+	cv::bilateralFilter(img2, img, d, sigmaColor, sigmaSpace);
 	ofxCv::toOf(img, image);
 
 	image.setImageType(OF_IMAGE_COLOR); // should not need this? TODO any over-head / conversion?
 
-	// bugbug support warm, cools?
+										// bugbug drop light and dark, use "if light then set lightthresh hold" cleans it all up
+										// bugbug support warm, cools?
 
 	readColors();
-
-	// bugbug drop light and dark, use "if light then set lightthresh hold" cleans it all up
-
-
-#ifdef DEBUG2
-	// sort results, this enables more flexibilty at run time
-	sort(colors.begin(), colors.end(), [=](std::pair<ofColor, int>& a, std::pair<ofColor, int>& b) {
-		// push less saturated back, then darks
-		if (a.first.getSaturation() == b.first.getSaturation()) {
-			if (a.first.getBrightness() == b.first.getBrightness()) {
-				return a.first.getLightness() > b.first.getLightness();
-			}
-			return a.first.getBrightness() > b.first.getBrightness();
-		}
-		return a.first.getSaturation() < b.first.getSaturation();
-		/* for debug
-		if (a.first.r == b.first.r)
-		if (a.first.g == b.first.g)
-		return a.first.b > b.first.b;
-		else
-		return  a.first.g > b.first.g;
-		else
-		return  a.first.r > b.first.r;
-
-		*/
-
-
-	}
-	);
-#endif // DEBUG2
 
 	count = shapes.size();
 
@@ -238,7 +245,7 @@ void ofApp::setup() {
 	finder.setThreshold(threshold);
 	finder.setUseTargetColor(true);
 	finder.setFindHoles(findHoles);// matters
-	for (int i = 0; i < count; ++i){
+	for (int i = 0; i < count; ++i) {
 		//bugbug move this to a function that can be called at any time to reset things
 		//bugbug save getPolylines in a file so redraw is fast
 		// install backup software
@@ -260,10 +267,63 @@ void ofApp::setup() {
 	for (map<int, colorData>::iterator it = shapes.begin(); it != shapes.end(); ++it) {
 		drawingData.push_back(it->second);
 	}
-	sort(drawingData.begin(), drawingData.end(), [=](colorData&  a, colorData&  b)	{
+	sort(drawingData.begin(), drawingData.end(), [=](colorData&  a, colorData&  b) {
 		return a.color.getSaturation() > a.color.getSaturation();
 		//bugbug need to add in sort by size, count, saturation, brightness, object size etc
 	});
+
+
+}
+
+void LiveArt::update() {
+	image.update();
+}
+void LiveArt::draw() {
+	ofSetColor(targetColor);
+	ofDrawRectangle(0, 700, 64, 64);
+
+	ofSetColor(ofColor::white);
+	ofSetBackgroundColor(warm);//bugbug use lightest found color
+
+	ofSetLineWidth(1);
+
+	image.draw(xImage, 0);// test with 2000,2000 image
+
+						  //ofTranslate(300, 0); keep as a reminder
+	if (index >= 0) {
+		ofPushStyle();
+		// less colors, do not draw on top of each other, find holes
+		ofSetColor(drawingData[index].color); // varibles here include only show large, or smalll, to create different pictures
+		savedcolors.push_back(drawingData[index].color);
+		echo(drawingData[index].lines);
+		if (++index >= count) {
+			index = -1; // stop
+		}
+		ofPopStyle();
+	}
+
+
+}
+void LiveArt::echo(vector<ofPolyline>&lines) {
+
+	for (int j = 0; j <lines.size(); j++) {
+		ofPolyline line = lines[j].getSmoothed(2); //bugbug test this data
+		ofTessellator tess;
+		ofMesh mesh;
+		tess.tessellateToMesh(line, OF_POLY_WINDING_ODD, mesh, true);
+		mesh.draw();
+		line.draw();
+	}
+}
+
+void ofApp::setup() {
+	cam.setup(640, 480);
+
+	ofSetFrameRate(120);
+	
+	art.setup();
+	art.setMenu(gui);
+
 
 	ofSetBackgroundAuto(false);
 }// 45shavlik11
@@ -271,7 +331,7 @@ void ofApp::setup() {
  //http://www.autobotic.com.my/ds3218-servo-control-angle-180-degree-25t-servo-armv
 void ofApp::update() {
 	cam.update();
-	image.update();
+	art.update();
 
 	//convertColor(cam, gray, CV_RGB2GRAY);
 	//GaussianBlur(gray, gray, gray.getWidth());
@@ -285,23 +345,8 @@ void ofApp::update() {
 
 
 }
-void ofApp::echo(vector<ofPolyline>&lines) {
-
-	for (int j = 0; j <lines.size(); j++) {
-		ofPolyline line = lines[j].getSmoothed(2); //bugbug test this data
-		ofTessellator tess;
-		ofMesh mesh;
-		tess.tessellateToMesh(line, OF_POLY_WINDING_ODD, mesh, true);
-		mesh.draw();
-		line.draw();
-	}
-}
 void ofApp::draw() {
-	ofSetColor(targetColor);
-	ofDrawRectangle(0, 700, 64, 64);
-
-	ofSetColor(ofColor::white);
-	ofSetBackgroundColor(warm);//bugbug use lightest found color
+	art.draw();
 	gui.draw();
 
 	//ofFill();
@@ -309,47 +354,24 @@ void ofApp::draw() {
 	//gray.draw(0, 480);(
 	//edge.draw(640, 0);
 	//sobel.draw(640, 480);
-	ofSetLineWidth(1);
-
-	image.draw(xImage, 0);// test with 2000,2000 image
-
-	//ofTranslate(300, 0); keep as a reminder
-	if (index >= 0) {
-		ofPushStyle();
-		// less colors, do not draw on top of each other, find holes
-		ofSetColor(drawingData[index].color); // varibles here include only show large, or smalll, to create different pictures
-		savedcolors.push_back(drawingData[index].color);
-		echo(drawingData[index].lines);
-		if (++index >= count) {
-			index = -1; // stop
-		}
-		ofPopStyle();
-	}
-	return;
 
 }
 void ofApp::keyPressed(int key) {
 	if (key == ' ') {
-		if (index == -1) {
-			index = savex;
+		if (art.index == -1) {
+			art.restore();
 		}
 		else {
-			savex = index;
-			index = -1;
-
-			string filename = "data" + ofToString(savecount++);
-			filename += ".dat";
-			toFile(filename, savedcolors, true);
-			savedcolors.clear();
+			art.snapshot();
 		}
 	}
 	else if (key == 'g') {
-		index = 0; // go from start
+		art.index = 0; // go from start
 	}
 	else if (key == 'b') {
-		index -= 20; // hit b a bunch of times to get back to the start
-		if (index < 0) {
-			index = 0;
+		art.index -= 20; // hit b a bunch of times to get back to the start
+		if (art.index < 0) {
+			art.index = 0;
 		}
 	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,60 +7,81 @@
 #include <unordered_set>
 using namespace ofxCv;
 using namespace cv;
-class colorData {
+class LiveArt {
 public:
-	colorData() {}
-	colorData(const ofColor& data) { color = data;  } //bugbut not everything is copied
-	int operator==(const colorData& data) { return data.color == color; }
-	ofColor color;
-	float threshold = 0;
-	vector<ofPolyline> lines;
-};
-class ofApp : public ofBaseApp {
-public:
-	void echo(vector<ofPolyline>&lines);
-	bool find(ofColor&color, bool add);
-	bool test(ofColor&color, int i, int j, int k);
-	bool dedupe(ofColor&color, int rangeR, int rangeG, int rangeB);
-	void readColors();
-	void keyPressed(int key);
+	class colorData {
+	public:
+		colorData() {}
+		colorData(const ofColor& data) { color = data; } //bugbut not everything is copied
+		int operator==(const colorData& data) { return data.color == color; }
+		ofColor color;
+		float threshold = 0;
+		vector<ofPolyline> lines;
+	};
 
 	void setup();
 	void update();
 	void draw();
 
-	ofVideoGrabber cam;
-	ofImage gray, edge, sobel;
-	ofImage image;
+	void setMenu(ofxPanel &gui);
+	void echo(vector<ofPolyline>&lines);
+	bool find(ofColor&color, bool add);
+	bool test(ofColor&color, int i, int j, int k);
+	bool dedupe(ofColor&color, int rangeR, int rangeG, int rangeB);
+	void readColors();
 	
-	cv::Mat img;
-	ofxPanel gui;
-	ofParameterGroup group;
-	ofParameter<float> threshold;
-	ofParameter<int> count;
-	ofParameter<int> index;
-	ofParameter<ofColor>targetColor;
-	ofColor warm;
+	void toFile(string path, vector<std::pair<ofColor, int>>&dat);
+	void toFile(string path, vector<ofColor>&dat, bool clear);
+	void fromFile(string path, vector<ofColor> &dat);
+	static bool isCool(ofColor&color);
 	vector<colorData> drawingData;
 
 	// read from xml file, 'r' key will refresh data? 
 
-	float minRadius = 1;
-	float maxRadius = 150;
-	bool findHoles = true;
-	int smoothingSize=2;//learn
-	float smoothingShape = 0.0;//bugbug learn
-	float xImage = 500;// make bigger when presenting, smaller when getting colors
-	float yImage = 500;
-	int d = 15; //bugbug learn for bilateralfilter
-	double sigmaColor = 80; //bugbug learn for bilateralfilter
-	double sigmaSpace = 80; //bugbug learn for bilateralfilter
-	string imagePath= "photo2.jpg";
+	ofParameter<float> minRadius = 1;
+	ofParameter<float> maxRadius = 150;
+	ofParameter<bool> findHoles = true;
+	ofParameter<int> smoothingSize = 2;//learn
+	ofParameter<float> smoothingShape = 0.0;//bugbug learn
+	ofParameter<float> xImage = 500;// make bigger when presenting, smaller when getting colors
+	ofParameter<float> yImage = 500;
+	ofParameter<int> d = 15; //bugbug learn for bilateralfilter
+	ofParameter<double> sigmaColor = 80; //bugbug learn for bilateralfilter
+	ofParameter<double> sigmaSpace = 80; //bugbug learn for bilateralfilter
+	ofParameter<string> imagePath = "photo2.jpg";
+
+	ofParameter<float> threshold;
+	ofParameter<int> count;
+	ofParameter<int> index;
+	ofParameter<ofColor>targetColor;
+	ofParameter<ofColor>warm;
+
+	ofImage image;
+	cv::Mat img;
+	
+	void snapshot();
+	void restore(){ index = savex; }
 
 private:
-	vector<ofColor> savedcolors; //bugbug make unique, right now will store dupes
 	map<int, colorData> shapes; //optimization
+	vector<ofColor> savedcolors; //bugbug make unique, right now will store dupes
+	int savex = 0;
 	int savecount = 0;
-	int savex;
+
+};
+class ofApp : public ofBaseApp {
+public:
+
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+	ofVideoGrabber cam;
+	ofImage gray, edge, sobel;
+	ofxPanel gui;
+	LiveArt art;
+
+private:
+
 };
 
